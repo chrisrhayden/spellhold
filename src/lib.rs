86fbc3ac764fe99046308cf89ff3c071764fe85b
chris <chrisrhayden@gmail.com>
2019-01-05 17:09:03 -0800

    adding connect evt

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
-// extern crate spellhold;
-
 pub mod listener_socket;
 pub mod client_socket;
 pub mod daemon;
 
-
 #[derive(Debug)]
 pub enum SendEvt {
     Kill,
-    SendString(String),
-    Err(String),
     None,
+    Err(String),
+    Connect(String),
+    SendString(String),
 }
-
