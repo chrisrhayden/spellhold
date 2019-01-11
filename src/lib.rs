cb6ceb99a8d6a4d5098bd4a2e72e93036e2b817f
chris <chrisrhayden@gmail.com>
2019-01-11 00:09:55 -0800

    removing unessisery enum varinats

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,31 @@
-pub mod listener_socket;
 pub mod daemon;
+pub mod unix_socket_handler;
 
-#[derive(Debug)]
+// TODO: make error more rusty
+#[derive(Debug, Clone)]
 pub enum SendEvt {
+    End,
     Kill,
     None,
-    Err(String),
     Connect(String),
     SendString(String),
+}
+
+impl SendEvt {
+    pub fn new(line: String) -> SendEvt {
+        SendEvt::evt_dispatch(line)
+    }
+
+    fn evt_dispatch(line: String) -> SendEvt {
+        if line.starts_with("kill") {
+            SendEvt::Kill
+        } else if line.starts_with("end") {
+            SendEvt::End
+        } else if !line.is_empty() {
+            SendEvt::SendString(line)
+        } else {
+            // idk how well get here
+            SendEvt::None
+        }
+    }
 }
